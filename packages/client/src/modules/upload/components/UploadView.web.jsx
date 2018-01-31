0f35d2dbefd3f0a800d877cef1f4a4ae316c68a9
Jakk S <jakkrit@Apples-iMac.local>
2018-01-31 15:29:19 +0700

    add axios

--- packages/client/src/modules/upload/components/UploadView.web.jsx
+++ packages/client/src/modules/upload/components/UploadView.web.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import Dropzone from 'react-dropzone';
 import filesize from 'filesize';
+import axios from 'axios';
 import { PageLayout, Row, Col, Table, Button, Alert } from '../../common/components/web';
 import settings from '../../../../../../settings';
 
@@ -13,10 +14,16 @@ export default class UploadView extends React.PureComponent {
     removeFile: PropTypes.func.isRequired
   };
 
-  state = {
-    error: null
-  };
+  constructor(props) {
+    super(props);
+    this.fetchImage = this.fetchImage.bind(this);
+  }
 
+  state = {
+    error: null,
+    returnedObject: null
+  };
+  url = 'http://203.154.245.241:9080/powerai-vision/temp/ee4749bb-0e0f-45a4-8d58-e9c0eab9f726.jpg';
   renderMetaData = () => (
     <Helmet
       title={`${settings.app.name} - Upload`}
@@ -35,6 +42,7 @@ export default class UploadView extends React.PureComponent {
       this.setState({ error: result.error });
     } else {
       this.setState({ error: null });
+      this.fetchImage(this.url);
     }
   };
 
@@ -48,13 +56,38 @@ export default class UploadView extends React.PureComponent {
     }
   };
 
+  fetchRealtimeFeed = feed => console.log(feed);
+
+  fetchImage = imageUrl => {
+    axios
+      .get(
+        `http://203.154.245.241:9080/powerai-vision/api/dlapis/0572bb30-328b-4ecb-8694-544ad3c0be57?imageUrl=${imageUrl}`
+      )
+      .then(response => {
+        let json = JSON.stringify(response.data.classified);
+        let jsonParsed = JSON.parse(json);
+        let { confidence, label, xmax, ymax, xmin, ymin } = jsonParsed[0];
+        console.log(confidence);
+        console.log(label);
+        console.log(xmax);
+        console.log(ymax);
+        console.log(xmin);
+        console.log(ymin);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
     const { files, uploadFiles } = this.props;
     const { error } = this.state;
-
+    console.log(files);
+    console.log('******');
+    console.log(uploadFiles);
     const columns = [
       {
-        title: 'Name',
+        title: 'ชื่อไฟล์',
         dataIndex: 'name',
         key: 'name',
         render: (text, record) => (
@@ -64,12 +97,12 @@ export default class UploadView extends React.PureComponent {
         )
       },
       {
-        title: 'Actions',
+        title: 'ลบ',
         key: 'actions',
         width: 50,
         render: (text, record) => (
-          <Button color="primary" size="sm" className="delete-button" onClick={() => this.hendleRemoveFile(record.id)}>
-            Delete
+          <Button color="danger" size="sm" className="delete-button" onClick={() => this.hendleRemoveFile(record.id)}>
+            ลบ
           </Button>
         )
       }
